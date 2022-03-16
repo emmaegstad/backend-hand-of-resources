c5f1ea9eb98150257754e6ea47566f2e44b2abba
Emma Egstad <emma@egstad.com>
2022-03-16 16:49:02 -0500

    passing tests for cats insert and findAll

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Cat = require('../lib/models/Cat');
 
 describe('backend-hand-of-resources routes', () => {
   beforeEach(() => {
@@ -21,5 +22,12 @@ describe('backend-hand-of-resources routes', () => {
     const res = await request(app).post('/api/v1/cats').send(expected);
 
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  it('gets a list of cats', async () => {
+    const expected = await Cat.findAll();
+    const res = await request(app).get('/api/v1/cats');
+
+    expect(res.body).toEqual(expected);
   });
 });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,16 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS cats;
+
+CREATE TABLE cats (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INT NOT NULL,
+    color TEXT NOT NULL
+);
+
+INSERT INTO
+    cats (name, age, color)
+VALUES
+    ('Minnie', 7, 'Brown Tabby'),
+    ('JJ', 7, 'Grey and White');
