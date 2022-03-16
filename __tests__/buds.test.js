f39dbc30aed402e0f00c2ed232580faea40576af
Emma Egstad <emma@egstad.com>
2022-03-16 18:28:50 -0500

    passing test for buds deleteById

--- __tests__/buds.test.js
+++ __tests__/buds.test.js
@@ -47,4 +47,11 @@ describe('bud routes', () => {
 
     expect(res.body).toEqual(expected);
   });
+
+  it('deletes a bud by id', async () => {
+    const expected = await Bud.findById(1);
+    const res = await request(app).delete(`/api/v1/buds/${expected.id}`);
+
+    expect(res.body).toEqual(expected);
+  });
 });
